8322e6c14676835c076f7e9573564ba2759581bc
chilakasaicharan99-hue <chilakasaicharan99@gmail.com>
2026-09-24 20:28:09 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Java application is running successfully.");
+        System.out.println("Jenkins build completed successfully.");
+    }
+}
